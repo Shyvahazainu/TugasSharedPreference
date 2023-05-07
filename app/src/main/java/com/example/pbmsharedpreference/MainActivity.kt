d24a1267e5908cb53bb71051df2866f6972a55bc
unknown <shyva.b20@mhs.unsyiah.ac.id>
2023-05-08 02:25:49 +0700

    Shared Preference fix

--- app/src/main/java/com/example/pbmsharedpreference/MainActivity.kt
+++ app/src/main/java/com/example/pbmsharedpreference/MainActivity.kt
@@ -4,28 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import androidx.preference.PreferenceManager
 
+
+//Nama : Shyva Annisa Hazainu
+//NPM : 2008107010055
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get the button
-        val btnPrintPrefs = findViewById<Button>(R.id.btn_print_prefs)
+        //mendapatkan nilai yang dimasukkan pengguna ke
+        // dalam EditText dan mencetaknya ke console saat tombol ditekan
 
-        // Set a click listener for the button
+        //untuk button berdasarkan id yang kita buat di activity_main
+        val btnPrintPrefs = findViewById<Button>(R.id.btnPrint)
+
+        // buat fungsi click pada button sehingga setelah ditekan
+        // akan muncul value pada console
         btnPrintPrefs.setOnClickListener {
-            // Get the username and password from the shared preferences
+            // untuk mengambil username dan password dari shared preferencenya
             val usernameEditText = findViewById<EditText>(R.id.editText)
             val passwordEditText = findViewById<EditText>(R.id.editText2)
 
-// Get the values from the EditText
+            // ambil value dari editText
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-// Print the values to the console
+            // print value-value tersebut ke console
             println("Username: $username")
             println("Password: $password")
         }
